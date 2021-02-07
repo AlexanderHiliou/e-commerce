bbf10d305cd441ce89c5e669bfc46dbd48b3985a
Alexander Hiliou <gilevsasha17@gmail.com>
2021-02-07 22:53:17 +0300

    view and template for cart

--- eccom_shop/settings.py
+++ eccom_shop/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'apps.product.context_processors.menu_category',
+                'apps.cart.context_processors.cart',
             ],
         },
     },
